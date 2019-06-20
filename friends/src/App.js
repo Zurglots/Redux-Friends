63d60d1679f366ae5f3c5ce5ce161c7fd7e6615a
Zurglots <bfeole@gmail.com>
2019-06-20 18:56:36 -0400

    Reducer FriendsList LoginPage

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import LoginPage from "./components/LoginPage";
+import FriendsList from "./components/FriendsList";
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends List</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/friends" component={FriendsList} />
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { login } from "../actions";
 
-const LoginPage = () => {
-  return (
-    <div>
+class LoginPage extends Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    this.props.login(this.state.credentials).then(res => {
+      if (res) {
+        this.props.history.push("/friends");
+      }
+    });
+  };
+
+  render() {
+    return (
       <div>
         <h2>Login</h2>
-        <form>
-          <input />
-          <input />
-          <button>Submit</button>
+        <form onSubmit={this.props.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button />
         </form>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default LoginPage;
+// const mapStateToProps = state => ({
+//   error: state.error,
+//   loggingIn: state.logginIn
+// });
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
